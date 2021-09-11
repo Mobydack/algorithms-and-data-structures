d18f4216b2198bd50fddea818a2104b7d877089a
Mobydack <devzetsubou@gmail.com>
2021-09-11 22:58:22 +0300

    solving the problem of the sum of two numbers

---
+++ 1/README.md
@@ -0,0 +1,6 @@
+# "a + b"
+
+- Language: C
+
+Write a function that calculates the sum of the two numbers
+
---
+++ 1/sum.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long int my_sum(long long int a, long long int b);
+
+int main(void)
+{
+	FILE *fp_read, *fp_write;
+	long long int num_1, num_2;
+
+	fp_read = fopen("input.txt", "r");
+
+	if (fp_read == NULL)
+	{
+		return 1;
+	}
+
+	fscanf(fp_read, "%ld", &num_1);
+	fscanf(fp_read, "%ld", &num_2);
+
+	fclose(fp_read);
+
+	const long long int result = my_sum(num_1, num_2);
+
+	fp_write = fopen("output.txt", "w+");
+
+	const char *result_buffer = malloc(sizeof result + 1);
+
+	sprintf(result_buffer, "%ld", result);
+	fputs(result_buffer, fp_write);
+	fclose(fp_write);
+
+	return 0;
+}
+
+
+long long int my_sum(long long int a, long long int b)
+{
+	return a + b;
+}
+
