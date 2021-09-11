2069c35de7b06eb4e2a864764c9825c7f283a9e2
Mobydack <devzetsubou@gmail.com>
2021-09-11 23:31:17 +0300

    add solved for second task

---
+++ 2/README.md
@@ -0,0 +1,7 @@
+# a + b^2
+
+
+- Language: C
+
+
+Write a function for solved next expression: `a + b^2`
---
+++ 2/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long int my_sum(long long int a, long long int b);
+
+int main(void)
+{
+	FILE *fp_read, *fp_write;
+	long long int num_1, num_2;
+
+	fp_read = fopen("input.txt", "r");
+
+	if (fp_read == NULL)
+		{
+			return 1;
+		}
+
+	fscanf(fp_read, "%lld", &num_1);
+	fscanf(fp_read, "%lld", &num_2);
+
+	fclose(fp_read);
+
+	const long long int result = my_sum(num_1, num_2);
+
+	fp_write = fopen("output.txt", "w+");
+
+	const char *result_buffer = malloc(sizeof result + 1);
+
+	sprintf(result_buffer, "%lld", result);
+	fputs(result_buffer, fp_write);
+	fclose(fp_write);
+
+	return 0;
+}
+
+
+long long int my_sum(long long int a, long long int b)
+{
+	return a + b * b;
+}
